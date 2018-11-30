e05d90d13d2080cf9c06d851c56ca80d977f7e8e
BloodyPig <ximuning213@163.com>
2018-11-30 16:55:30 +0800

    添加了设置类和飞船类

--- aline_invasion.py
+++ aline_invasion.py
@@ -1,15 +1,17 @@
 import sys
 import pygame
-
+from settings import Settings
+from ship import Ship
 
 def run_game():
-    # 初始化游戏 创建屏幕对象
+    # 初始化pygame、设置、屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Aline Invasion")
 
-    # 设置背景色
-    bg_color = (230, 230, 230)
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     # 游戏主循环
     while True:
@@ -18,8 +20,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        # 每次循环都绘制背景色
-        screen.fill(bg_color)
+        # 每次循环都重绘屏幕
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
