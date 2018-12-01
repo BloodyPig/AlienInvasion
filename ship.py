746b16bb7700b5adcd03f36f03dacaa5e760c871
BloodyPig <ximuning213@163.com>
2018-12-01 13:55:28 +0800

    飞船实体类，新增了左右移动和边界控制

--- ship.py
+++ ship.py
@@ -1,12 +1,14 @@
 import pygame
 
+
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         ship_path = './spaceship.bmp'
+
         # 初始化飞船并设置其初始位置
         self.screen = screen
-
+        self.ai_settings = ai_settings
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load(ship_path)
         self.rect = self.image.get_rect()
@@ -15,6 +17,23 @@ class Ship():
         # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # 在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
+
+        # 设置飞船的移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # 根据移动标志调整飞船的位置,并控制边界
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # 更新rect对象
+        self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
