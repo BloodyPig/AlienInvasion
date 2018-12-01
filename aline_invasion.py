5a5dd2d8a8fa200256c59cc2558941471ee7bbaf
BloodyPig <ximuning213@163.com>
2018-12-01 13:53:01 +0800

    重构了时间响应模块，添加成为game_functions类

--- aline_invasion.py
+++ aline_invasion.py
@@ -2,30 +2,25 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
+
 
 def run_game():
     # 初始化pygame、设置、屏幕对象
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Aline Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # 游戏主循环
     while True:
-
-        # 监视键鼠事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # 每次循环都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
