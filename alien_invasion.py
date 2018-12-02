8dfcef981acfd5e3dbde643f5a1d87732a22683b
BloodyPig <ximuning213@163.com>
2018-12-02 12:20:20 +0800

    游戏入口

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
+from button import Button
 
 
 def run_game():
@@ -14,19 +16,27 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Aline Invasion")
 
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
     # 创建一艘飞船、存储子弹数量的编组、外星人编组
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    # 创建外星人群
     gf.create_fleet(ai_settings, screen, ship, aliens)
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
 
     # 游戏主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.check_collosion(bullets, aliens)
+            gf.update_bullets(bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, stats, screen, ship,
+                         aliens, bullets, play_button)
 
 
 run_game()
