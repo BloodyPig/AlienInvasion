395a7f91a75cb1cef678de1e9db6bc80f8250f44
BloodyPig <ximuning213@163.com>
2018-12-02 12:20:47 +0800

    过程控制函数

--- game_functions.py
+++ game_functions.py
@@ -25,7 +25,7 @@ def chek_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     # 监视键鼠事件
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,6 +35,24 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             chek_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    # 单击按钮后开始游戏
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
+
+
+def check_collosion(bullets, aliens):
+    # 检测子弹和外星人的碰撞并删除
+    for bullet in bullets:
+        for alien in aliens:
+            if pygame.sprite.collide_rect(bullet, alien):
+                bullets.remove_internal(bullet)
+                aliens.remove_internal(alien)
 
 
 def update_bullets(bullets):
@@ -48,6 +66,8 @@ def update_bullets(bullets):
             # bullets.remove(bullet)
             bullets.remove_internal(bullet)
     # print(len(bullets)) # 设置的查看是否正确消失的标志
+    # 检测组碰撞代码不可用
+    # collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
@@ -97,12 +117,55 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         bullets.add_internal(new_bullet)
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def check_fleet_edges(ai_settings, aliens):
+    # 外星人到达边界的处理
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    # 整群外星人下移，并改变其方向
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    # 相应飞船被外星人撞到
+    if stats.ships_left > 0:
+        # 生命减一
+        stats.ships_left -= 1
+        for bullet in bullets:
+            bullets.remove_internal(bullet)
+        for alien in aliens:
+            aliens.remove_internal(alien)
+        # 重新开始游戏
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+    else:
+        stats.game_active = False
+
+
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+    # 更新所有外星人的位置
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+    # 检测外星人和飞船的碰撞
+    if pygame.sprite.spritecollideany(ship, aliens):
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+
+def update_screen(ai_settings, stats, screen, ship, aliens, bullets, play_button):
     # 每次循环都重绘屏幕
     screen.fill(ai_settings.bg_color)
     ship.blitme()
     aliens.draw(screen)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
+
+    if not stats.game_active:
+        play_button.draw_button()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
