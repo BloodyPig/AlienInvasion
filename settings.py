76cdb77daf27886367f4f84e951a583b67fbd1e4
BloodyPig <ximuning213@163.com>
2018-12-02 12:21:24 +0800

    设置信息类

--- settings.py
+++ settings.py
@@ -5,8 +5,13 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (250,250,250)
         self.ship_speed_factor = 3
+        self.ship_limit = 3
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 5
         self.bullet_color = 60,60,60
         self.bullet_allowed = 5
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # 1:右移 -1:左移
+        self.fleet_direction = 1
